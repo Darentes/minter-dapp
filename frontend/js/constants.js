fa8cbd434b1ece6c667ba3efa8b0fecaa9f84efe
Darentes <tresmusicpay@gmail.com>
2022-05-11 04:42:18 +0200

    updates

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Mynies NFT Project!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'Mynies is a comic-based NFT project, which are also the cutest collection on the blockchain. Are you ready? >.<" ';
+const welcome_p = 'Mynies is a comic-based NFT project, which are also will be the cutest collection in the blockchain. Are you ready? >.<" ';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
@@ -11,15 +11,15 @@ const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "DM us at Twitter or Instagram to get on the Whitelist";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. Message us at Twitter or Instagram to get on the Whitelist!";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale!";
+const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you!";
+const p_public_mint = "No whitelist needed. Public minting is now open!";
 const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Special NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const mint_failed = "Minting failed. Please try again.";
 
 const abi = [
   {
